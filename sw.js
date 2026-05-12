cdc1572b97dd59ab4e2c3d0069380d720eed5ead
hamaclem <hanna.clemen@web.de>
2026-05-12 21:09:07 +0200

    change icons, add shake function

--- sw.js
+++ sw.js
@@ -1,19 +1,67 @@
+const CACHE_NAME = "boggle-cache";
+
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./sw.js",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+// INSTALL
 self.addEventListener("install", (event) => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open("boggle-cache").then((cache) => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json"
-      ]);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(APP_SHELL);
     })
   );
 });
 
+// ACTIVATE
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// FETCH
 self.addEventListener("fetch", (event) => {
+  const request = event.request;
+
+  // Navigation (index.html) IMMER zuerst aus Cache
+  if (request.mode === "navigate") {
+    event.respondWith(
+      caches.match("./index.html").then((cached) => {
+        return cached || fetch(request);
+      })
+    );
+    return;
+  }
+
+  // Rest: Cache First + Update
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+    caches.match(request).then((cached) => {
+      const fetchPromise = fetch(request)
+        .then((networkResponse) => {
+          return caches.open(CACHE_NAME).then((cache) => {
+            cache.put(request, networkResponse.clone());
+            return networkResponse;
+          });
+        })
+        .catch(() => cached);
+
+      return cached || fetchPromise;
     })
   );
 });
